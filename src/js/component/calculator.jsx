7789fdf78578a6c39f73c86a78ec43d30c6736df
FrancescaAmeche <121627825+FrancescaAmeche@users.noreply.github.com>
2023-09-14 02:31:25 +0000

    added styles to css, started useState functions

--- src/js/component/calculator.jsx
+++ src/js/component/calculator.jsx
@@ -1,41 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Calculator = () => {
 
+    const [calculator, setCalculator] = useState(0);
+    // const [numbers, setNumbers] = useState();
+
+    // function calculatorDisplay() {
+    //     setNumbers()
+    // }
+
+
     return (
         <div className="container">
-            <div className="position-relative" style={{width: "1300px", height: "800px", marginTop: "100px", backgroundColor: "#51A9AC"}}>
-                <div className="bg-dark rounded-4 position-absolute top-50 start-50 translate-middle text-white" style={{width: "400px", height: "605px"}}>
-                    <div className="bg-secondary d-flex justify-content-end pe-4 m-2 rounded-2 fs-1 pt-3" style={{width: "385px", height: "100px"}}>0</div>
-                    <div className="d-flex flex-row">
-                        <button className="ms-2 rounded-2 text-white fs-2" style={{width: "90px", height: "90px", backgroundColor: "#8B51AC"}}>C</button>
-                        <button className="ms-2 rounded-2 text-white fs-2" style={{width: "90px", height: "90px", backgroundColor: "#8B51AC"}}>+-</button>
-                        <button className="ms-2 rounded-2 text-white fs-2" style={{width: "90px", height: "90px", backgroundColor: "#8B51AC"}}>%</button>
-                        <button className="ms-2 rounded-2 text-white fs-2" style={{width: "90px", height: "90px", backgroundColor: "#8B51AC"}}>/</button>
+            <div className="blueBox position-relative">
+                <div className="bigBox bg-dark position-absolute top-50 start-50 translate-middle">
+                    <div>
+                        <input value={calculator} onChange={(e) => {setCalculator(e.target.value)}} type="text" className="display bg-secondary d-flex justify-content-end pe-4 m-2 fs-1 pt-3"></input>
                     </div>
                     <div className="d-flex flex-row">
-                        <button className="ms-2 mt-2 rounded-2 text-white fs-2" style={{width: "90px", height: "90px", backgroundColor: "#8B51AC"}}>7</button>
-                        <button className="ms-2 mt-2 rounded-2 text-white fs-2" style={{width: "90px", height: "90px", backgroundColor: "#8B51AC"}}>8</button>
-                        <button className="ms-2 mt-2 rounded-2 text-white fs-2" style={{width: "90px", height: "90px", backgroundColor: "#8B51AC"}}>9</button>
-                        <button className="ms-2 mt-2 rounded-2 text-white fs-2" style={{width: "90px", height: "90px", backgroundColor: "#8B51AC"}}>x</button>
+                        <button className="purpleButton ms-2 fs-2">C</button>
+                        <button className="purpleButton ms-2 fs-2">+-</button>
+                        <button className="purpleButton ms-2 fs-2">%</button>
+                        <button className="purpleButton ms-2 fs-2">/</button>
                     </div>
                     <div className="d-flex flex-row">
-                        <button className="ms-2 mt-2 rounded-2 text-white fs-2" style={{width: "90px", height: "90px", backgroundColor: "#8B51AC"}}>4</button>
-                        <button className="ms-2 mt-2 rounded-2 text-white fs-2" style={{width: "90px", height: "90px", backgroundColor: "#8B51AC"}}>5</button>
-                        <button className="ms-2 mt-2 rounded-2 text-white fs-2" style={{width: "90px", height: "90px", backgroundColor: "#8B51AC"}}>6</button>
-                        <button className="ms-2 mt-2 rounded-2 text-white fs-2" style={{width: "90px", height: "90px", backgroundColor: "#8B51AC"}}>-</button>
+                        <button className="purpleButton ms-2 mt-2 fs-2">7</button>
+                        <button className="purpleButton ms-2 mt-2 fs-2">8</button>
+                        <button className="purpleButton ms-2 mt-2 fs-2">9</button>
+                        <button className="purpleButton ms-2 mt-2 fs-2">x</button>
                     </div>
                     <div className="d-flex flex-row">
-                        <button className="ms-2 mt-2 rounded-2 text-white fs-2" style={{width: "90px", height: "90px", backgroundColor: "#8B51AC"}}>1</button>
-                        <button className="ms-2 mt-2 rounded-2 text-white fs-2" style={{width: "90px", height: "90px", backgroundColor: "#8B51AC"}}>2</button>
-                        <button className="ms-2 mt-2 rounded-2 text-white fs-2" style={{width: "90px", height: "90px", backgroundColor: "#8B51AC"}}>3</button>
-                        <button className="ms-2 mt-2 rounded-2 text-white fs-2" style={{width: "90px", height: "90px", backgroundColor: "#8B51AC"}}>+</button>
+                        <button className="purpleButton ms-2 mt-2 fs-2">4</button>
+                        <button className="purpleButton ms-2 mt-2 fs-2">5</button>
+                        <button className="purpleButton ms-2 mt-2 fs-2">6</button>
+                        <button className="purpleButton ms-2 mt-2 fs-2">-</button>
                     </div>
                     <div className="d-flex flex-row">
-                        <button className="ms-2 mt-2 rounded-2 text-white fs-2" style={{width: "90px", height: "90px", backgroundColor: "#8B51AC"}}>0</button>
-                        <button className="ms-2 mt-2 rounded-2 text-white fs-2" style={{width: "90px", height: "90px", backgroundColor: "#8B51AC"}}>.</button>
-                        <button className="ms-2 mt-2 rounded-2 text-white fs-2" style={{width: "188px", height: "90px", backgroundColor: "#B64097"}}>=</button>
-                        
+                        <button className="purpleButton ms-2 mt-2 fs-2">1</button>
+                        <button className="purpleButton ms-2 mt-2 fs-2">2</button>
+                        <button className="purpleButton ms-2 mt-2 fs-2">3</button>
+                        <button className="purpleButton ms-2 mt-2 fs-2">+</button>
+                    </div>
+                    <div className="d-flex flex-row">
+                        <button className="purpleButton ms-2 mt-2 fs-2">0</button>
+                        <button className="purpleButton ms-2 mt-2 fs-2">.</button>
+                        <button className="pinkButton ms-2 mt-2 fs-2">=</button>
                     </div>
                 </div>
             </div>
